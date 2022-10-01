c63fa4e1179a78491e81ce89d0448b6594617d73
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2022-10-01 12:35:02 +0330

    feat: remove adding device_credential

--- backend/app/app/api/api_v1/endpoints/device_credential.py
+++ backend/app/app/api/api_v1/endpoints/device_credential.py
@@ -29,62 +29,62 @@ def read_device_credentials(
     return objs
 
 
-@router.post("/", response_model=schemas.DeviceCreate)
-def create_device_credential(
-    *,
-    db: Session = Depends(deps.get_db),
-    obj_in: schemas.DeviceCredentialUpdate,
-) -> Any:
-    """
-    Create new device_credential.
-    """
-    obj = crud.device.get(db, id=obj_in.id)
-    if obj:
-        raise HTTPException(
-            status_code=400,
-            detail="The device with this external_id already exists in the system.",
-        )
-    obj = crud.device_credential.create(db, obj_in=obj_in)
-    return obj
+# @router.post("/", response_model=schemas.DeviceCreate)
+# def create_device_credential(
+#     *,
+#     db: Session = Depends(deps.get_db),
+#     obj_in: schemas.DeviceCredentialUpdate,
+# ) -> Any:
+#     """
+#     Create new device_credential.
+#     """
+#     obj = crud.device.get(db, id=obj_in.id)
+#     if obj:
+#         raise HTTPException(
+#             status_code=400,
+#             detail="The device with this id already exists in the system.",
+#         )
+#     obj = crud.device_credential.create(db, obj_in=obj_in)
+#     return obj
 
 
-@router.get("/{device_credential_id}", response_model=schemas.Device)
-def read_device_credentials(
-    device_credential_id: UUID,
-    current_user: models.User = Depends(deps.get_current_active_user),
-    db: Session = Depends(deps.get_db),
-) -> Any:
-    """
-    Get a specific device by id.
-    """
-    obj = crud.device_credential.get(db, id=device_credential_id)
-    if not crud.user.is_superuser(current_user):
-        raise HTTPException(
-            status_code=400, detail="The user doesn't have enough privileges"
-        )
-    return obj
+# @router.get("/{device_credential_id}", response_model=schemas.Device)
+# def read_device_credentials(
+#     device_credential_id: UUID,
+#     current_user: models.User = Depends(deps.get_current_active_user),
+#     db: Session = Depends(deps.get_db),
+# ) -> Any:
+#     """
+#     Get a specific device by id.
+#     """
+#     obj = crud.device_credential.get(db, id=device_credential_id)
+#     if not crud.user.is_superuser(current_user):
+#         raise HTTPException(
+#             status_code=400, detail="The user doesn't have enough privileges"
+#         )
+#     return obj
 
 
-@router.put("/{device_credential_id}", response_model=schemas.DeviceCredentialUpdate)
-def update_device(
-    *,
-    db: Session = Depends(deps.get_db),
-    device_credential_id: UUID,
-    obj_in: schemas.DeviceCredential,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
-) -> Any:
-    """
-    Update a device_credentials.
-    """
-    obj = crud.device_credential.get(db, id=device_credential_id)
-    if not obj:
-        raise HTTPException(
-            status_code=404,
-            detail="The device with this external_id does not exist in the system",
-        )
-    if not crud.user.is_superuser(current_user):
-        raise HTTPException(
-            status_code=400, detail="The user doesn't have enough privileges"
-        )
-    obj = crud.device_credential.update(db, db_obj=obj, obj_in=obj_in)
-    return obj
+# @router.put("/{device_credential_id}", response_model=schemas.DeviceCredentialUpdate)
+# def update_device(
+#     *,
+#     db: Session = Depends(deps.get_db),
+#     device_credential_id: UUID,
+#     obj_in: schemas.DeviceCredential,
+#     current_user: models.User = Depends(deps.get_current_active_superuser),
+# ) -> Any:
+#     """
+#     Update a device_credentials.
+#     """
+#     obj = crud.device_credential.get(db, id=device_credential_id)
+#     if not obj:
+#         raise HTTPException(
+#             status_code=404,
+#             detail="The device with this id does not exist in the system",
+#         )
+#     if not crud.user.is_superuser(current_user):
+#         raise HTTPException(
+#             status_code=400, detail="The user doesn't have enough privileges"
+#         )
+#     obj = crud.device_credential.update(db, db_obj=obj, obj_in=obj_in)
+#     return obj
